f8d49b0f18b6b174dc2b651e3d281a837317ac4d
Mathis Chenuet <artdevelopp@hotmail.fr>
2019-02-27 22:02:03 +0100

    parsing & get_rects

--- main.py
+++ main.py
@@ -5,11 +5,28 @@ import numpy as np
 from utils import *
 
 
+def get_rects(min_ingredients, max_cells):
+    min_cells = min_ingredients * 2
+    rects = []
+    for size in range(min_cells, max_cells + 1):
+        for rowspan in range(1, size):
+            if size % rowspan != 0:
+                continue
+            colspan = size // rowspan
+            rects.append((rowspan, colspan))
+    return rects
+
+
 def main():
     rows, columns, min_ingredients, max_cells = ints()
-    pizza = [input() for _ in range(rows)]
+    pizza = [list(input()) for _ in range(rows)]
 
-    err(pizza[0][:10], '...')
+    # err(pizza[0][:10], '...')
+    pizza = np.uint8(np.array(pizza) == 'M')
+    err(pizza)
+
+    rects = get_rects(min_ingredients, max_cells)
+    err(rects)
 
 
 if __name__ == '__main__':
