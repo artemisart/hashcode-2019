bd44f855673a7bfa80ba179f58d5e104a80ab6eb
milesial <milesial@users.noreply.github.com>
2019-02-27 20:34:41 +0100

    Valid rect utils && run.sh executable

--- utils.py
+++ utils.py
@@ -12,3 +12,19 @@ def ints():
 
 def floats():
     return [float(x) for x in input().split()]
+
+
+def is_valid_ingredients(rectangle, L):
+    assert rectangle.max() == 1
+    assert rectangle.min() == 0
+    return rectangle.sum() >= L and (1 - rectangle).sum() >= L
+
+
+def is_valid_size(rectangle, H):
+    assert rectangle.max() == 1
+    assert rectangle.min() == 0
+    return rectangle.size >= H
+
+
+def is_valid(rectangle, L, H):
+    return is_valid_ingredients(rectangle, L) and is_valid_size(rectangle, H)
