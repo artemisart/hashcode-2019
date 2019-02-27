f55b2ba8b5f1a874729e919f53174a9a3cfceee0
Mathis Chenuet <artdevelopp@hotmail.fr>
2019-02-27 21:09:45 +0100

    smaller output

--- main.py
+++ main.py
@@ -8,7 +8,8 @@ from utils import *
 def main():
     rows, columns, min_ingredients, max_cells = ints()
     pizza = [input() for _ in range(rows)]
-    err(pizza)
+
+    err(pizza[0][:10], '...')
 
 
 if __name__ == '__main__':
