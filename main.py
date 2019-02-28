8866c5f54ce0979c902703a98a8bcf0f36631e95
milesial <milesial@users.noreply.github.com>
2019-02-28 20:12:35 +0100

    Photo repr

--- main.py
+++ main.py
@@ -2,12 +2,21 @@
 
 from utils import *
 
+from milesial_vert_matcher import run
+
 
 class Photo:
     def __init__(self, vertical, tags, id):
         self.vertical = vertical
         self.tags = tags
         self.id = id
+
+    def __repr__(self):
+        o = 'H'
+        if self.vertical:
+            o = 'V'
+        return f'({o}, id:{self.id}, ntags:{len(self.tags)}, tags:{self.tags})'
+
 
 def parse_photo(line, id):
     h, _, *tags = line.split()
@@ -18,7 +27,9 @@ def main():
     photo_count = int(input())
     photos = [parse_photo(input(), i) for i in range(photo_count)]
     all_tags = set(tag for p in photos for tag in p.tags)
-    err('unique tags', len(all_tags))
+    run(photos)
+    # err('unique tags', len(all_tags))
+
 
 if __name__ == '__main__':
     main()
