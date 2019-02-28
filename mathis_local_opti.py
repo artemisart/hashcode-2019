e16525ccf5cd83cb4ebded841db0c574e24f2e4b
Mathis Chenuet <artdevelopp@hotmail.fr>
2019-02-28 23:14:27 +0100

    test local opti

--- mathis_local_opti.py
+++ mathis_local_opti.py
@@ -35,13 +35,30 @@ def main():
         else:
             slides.append([photo.id])
 
-    WINDOW = 5  # shuffle in WINDOW-2
-    STEP = 1
-    for i in range(0, len(slides) - WINDOW, STEP):
-        max_score = 0
-        for order in permutations(range(i + 1, i + WINDOW - 1)):
-            full = [i, *order, i + WINDOW - 1]
-            err('full', full)
+    ITER = 10
+    for iter in range(ITER):
+        WINDOW = 7  # shuffle in WINDOW-2
+        STEP = 1
+        for i in range(0, len(slides) - WINDOW, STEP):
+            max_score = None
+            max_order = None
+            for order in permutations(range(i + 1, i + WINDOW - 1)):
+                full_order = [i, *order, i + WINDOW - 1]
+                score = sum(
+                    calc_score(photos, slides[a], slides[b])
+                    for a, b in zip(full_order, full_order[1:])
+                )
+                # err('full', full_order, score)
+                if max_score is None:  # first time is in the original order
+                    max_score = score
+                elif score > max_score:
+                    err(f"improved score from {max_score} to {score}")
+                    max_score = score
+                    max_order = full_order
+            if max_order is not None:
+                reordered_slides = [slides[i] for i in max_order]
+                # err(reordered_slides)
+                slides[i : i + WINDOW] = reordered_slides
 
     print(len(slides))
     for slide in slides:
