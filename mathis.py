0d56406461637105e3221947a49e55fbb7460b38
Mathis Chenuet <artdevelopp@hotmail.fr>
2019-02-28 20:38:02 +0100

    update mathis.py

--- mathis.py
+++ mathis.py
@@ -13,15 +13,26 @@ def main():
     sys.stdout = open(out_file, 'w', encoding='utf8')
 
     photo_count = int(input())
-    photos = [parse_photo(input()) for _ in range(photo_count)]
+    photos = [parse_photo(input(), id) for id in range(photo_count)]
     all_tags = set(tag for p in photos for tag in p.tags)
     err('unique tags', len(all_tags))
 
-    slides = [i for i in range(photo_count) if photos[i].vertical == False]
+    # slides = [i for i in range(photo_count) if photos[i].vertical == False]
+    slides = []
+    last_vertical = None
+    for photo in photos:
+        if photo.vertical:
+            if last_vertical is None:
+                last_vertical = photo
+            else:
+                slides.append([last_vertical.id, photo.id])
+                last_vertical = None
+        else:
+            slides.append([photo.id])
 
     print(len(slides))
     for slide in slides:
-        print(slide)
+        print(*slide)
 
 
 if __name__ == '__main__':
