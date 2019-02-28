576d3af71447a352075393d4ddbd3790d430ed9c
ehariz <eliashrz@outlook.fr>
2019-02-28 19:59:58 +0100

    Added photo id

--- main.py
+++ main.py
@@ -1,27 +1,24 @@
 #!/usr/bin/env python
 
-import numpy as np
-
 from utils import *
 
 
 class Photo:
-    def __init__(self, vertical, tags):
+    def __init__(self, vertical, tags, id):
         self.vertical = vertical
         self.tags = tags
+        self.id = id
 
-
-def parse_photo(line):
+def parse_photo(line, id):
     h, _, *tags = line.split()
-    return Photo(h == 'V', set(tags))
+    return Photo(h == 'V', set(tags), id)
 
 
 def main():
     photo_count = int(input())
-    photos = [parse_photo(input()) for _ in range(photo_count)]
+    photos = [parse_photo(input(), i) for i in range(photo_count)]
     all_tags = set(tag for p in photos for tag in p.tags)
     err('unique tags', len(all_tags))
-
 
 if __name__ == '__main__':
     main()
